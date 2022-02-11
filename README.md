11d8f0a76f939f5b69d116f6880a454766ffed7e
Akshay Rajguru <akshaygururaj141@gmail.com>
2022-02-12 00:14:10 +0530

    create get and post apis

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-fast-api-course
+## 1. pip install fastapi[all]
+## 2. uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello World!!!"}
+    return {"message": "Hello World!"}
 
 
 @app.get("/items/{item_id}")
@@ -21,3 +22,13 @@ def read_item(item_id: int, q: Optional[str] = None):
     :return dict: {"item_id", "query"}
     """
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/posts/")
+async def get_posts():
+    return {"post": "this is the post 1"}
+
+
+@app.post("/api/create/post")
+async def create_post(body: dict = Body(...)):
+    return {"title": body["title"], "content": body["content"]}
