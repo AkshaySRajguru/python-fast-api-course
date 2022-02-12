66e2e4808391e0d9c8c815237fa2647e5d460493
Akshay Rajguru <akshaygururaj141@gmail.com>
2022-02-12 20:23:44 +0530

    implemented CRUD in memory

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from typing import Optional
+from random import randrange
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 # from fastapi.params import Body
 from pydantic import BaseModel
 
 
 class Post(BaseModel):
+    id: Optional[int] = None
     title: str
     content: str
     published: bool = True
@@ -13,6 +15,9 @@ class Post(BaseModel):
 
 
 app = FastAPI()
+
+my_posts = [{"id": 1, "title": "Covid vaccination", "content": "there are three types of vaccines"},
+            {"id": 2, "title": "HIS details", "content": "lot of beds available to associates"}]
 
 
 @app.get("/")
@@ -33,15 +38,71 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/api/posts/")
-async def get_posts():
-    return {"post": "this is the post 1"}
+@app.get("/api/get/all/posts/")
+async def get_all_posts():
+    return my_posts
 
+
+@app.get("/api/get/posts/{id}")
+async def get_posts(id: int):
+    try:
+        result = None
+        for post in my_posts:
+            if post['id'] == int(id):
+                result = post
+        if result:
+            return result
+        else:
+            return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id:{id} is not found!')
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_200_OK, detail=f'Error occurred: {e}')
 
 # @app.post("/api/create/posts")
 # async def create_post(body: dict = Body(...)):
 #     return {"title": body["title"], "content": body["content"]}
 
-@app.post("/api/create/posts")
+
+@app.post("/api/create/posts", status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post):
-    return post.dict()
+    latest_post = post.dict()
+    latest_post['id'] = randrange(0, 1000)
+    my_posts.append(latest_post)
+    return latest_post
+
+
+@app.put("/api/update/posts/", status_code=status.HTTP_200_OK)
+async def update_post(post: Post):
+    try:
+        post_dict = post.dict()
+        print(post_dict)
+        is_id_passed = "id" in post_dict.keys()
+        if not is_id_passed:
+            return HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                 detail=f'pass id of post in the body of the request!')
+        result = None
+        for i, p in enumerate(my_posts):
+            if p['id'] == int(post_dict["id"]):
+                my_posts[i] = post_dict
+                result = True
+        if result:
+            return {"message": "post updated successfully!", "data": post_dict}
+        else:
+            return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id:{id} is not found!')
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_200_OK, detail=f'Error occurred: {e}')
+
+
+@app.delete("/api/delete/posts/{id}", status_code=status.HTTP_200_OK)
+async def delete_post(id: int):
+    try:
+        result = None
+        for i, p in enumerate(my_posts):
+            if p["id"] == int(id):
+                my_posts.pop(i)
+                result = True
+        if result:
+            return {"message": f"post with id:{id} deleted successfully!"}
+        else:
+            return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id:{id} is not found!')
+    except Exception as e:
+        return HTTPException(status_code=status.HTTP_200_OK, detail=f'Error occurred: {e}')
